33a90ded4bb8aeafddc769bd4c3a33cdf714b6ac
tanishi <tatsunishi1031@yahoo.co.jp>
2020-02-21 22:30:14 +0900

    Update gemfile.lock for heroku

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -17,5 +19,7 @@ module SampleApp
 
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
   end
 end
+
