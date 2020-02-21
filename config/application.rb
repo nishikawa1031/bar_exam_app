876999671752108bc74e06572ba2a9aef098947c
tanishi <tatsunishi1031@yahoo.co.jp>
2020-02-21 16:47:57 +0900

    fix i18n

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module SampleApp
     # -- all .rb files in that directory are automatically loaded.
 
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
