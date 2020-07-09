594f9d388f26b0f98e163f396dd72160eb715fc7
tanishi <tatsunishi1031@yahoo.co.jp>
2020-07-09 20:00:25 +0900

    文言の修正

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,7 +7,7 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
+      flash[:success] = "答案が投稿できました!"
       redirect_to root_url
     else
       @feed_items = current_user.feed.paginate(page: params[:page])
