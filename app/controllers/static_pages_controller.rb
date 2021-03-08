bbd6892b8d1ee22f193ca663f1f6a764b8e32b5c
nishikawa1031 <tatsunishitatsu@gmail.com>
2021-03-09 08:50:49 +0900

    開発を再開

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,35 @@
+class StaticPagesController < ApplicationController
+  def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+
+    # app/views/リソース名/アクション名.html.erb
+    # app/views/static_pages/home.html.erb
+  end
+
+  # GET /static_pages/:name
+  def show
+    @subject = params[:name]    #カラムのsubjectとは、無関係のsubject。カラム名を取得するmoduleがある？
+    @year = params[:year_id]
+    if @year.nil?
+      @microposts = Micropost.where(subject: params[:name])
+    else
+      @microposts = Micropost.where(subject: params[:name],year: params[:year_id])
+    end
+  end
+
+
+
+  def help
+  end
+
+  def about
+    # 'app/views/static_pages/about.html.erb'
+  end
+
+  def contact
+    # app/views/static_pages/contact.html.erb'
+  end
+end
---
+++ app/models/micropost.rb
@@ -0,0 +1,40 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
+  validates :user_id, presence: true
+  validates :content, length: { maximum: 10000 }
+  validate  :picture_size
+  validates  :subject, presence: true
+  validates  :year, presence: true
+  
+  
+  enum subject:{
+    constitution: 0,
+    civil_law:1,
+    criminal_law:2,
+    civil_procedure_law:3,
+    criminal_procedure_law:4,
+    administrative_law:5,
+    commercial_law:6,
+    bankruptcy_law:7,
+    tax_law:8,
+    economic_law:9,
+    intellectual_property_law:10,
+    labor_law:11,
+    environmental_law:12,
+    international_private_law:13,
+    international_public_law:14,
+}
+
+
+
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 3.megabytes
+        errors.add(:picture, "should be less than 3MB")
+      end
+    end
+end
